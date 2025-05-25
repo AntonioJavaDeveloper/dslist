0896ff136983832a8309c9615999cab373c21d92
Antonio Gutierrez <antonio@javadeveloper.com.br>
2025-05-25 13:31:26 -0300

    Add findByGameList method in GameService to retrieve games by list ID using projection

--- src/main/java/br/com/javadeveloper/dslist/service/GameService.java
+++ src/main/java/br/com/javadeveloper/dslist/service/GameService.java
@@ -3,6 +3,7 @@ package br.com.javadeveloper.dslist.service;
 import br.com.javadeveloper.dslist.dto.GameDTO;
 import br.com.javadeveloper.dslist.dto.GameMinDTO;
 import br.com.javadeveloper.dslist.entities.Game;
+import br.com.javadeveloper.dslist.projections.GameMinProjection;
 import br.com.javadeveloper.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,14 +18,19 @@ public class GameService {
 
     @Transactional(readOnly = true)
     public GameDTO findById(Long id) {
-        Game result = gameRepository.findById(id).get();
+        Game result = this.gameRepository.findById(id).get();
         return new GameDTO(result);
     }
 
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
-        List<Game> result = gameRepository.findAll();
+        List<Game> result = this.gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
     }
 
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByGameList(Long listId) {
+        List<GameMinProjection> games = this.gameRepository.searchByList(listId);
+        return games.stream().map(GameMinDTO::new).toList();
+    }
 }
