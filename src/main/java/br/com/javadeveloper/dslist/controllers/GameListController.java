fc8efedf2adc094dc4b16a6429deccd3f75b3d8d
Antonio Gutierrez <antonio@javadeveloper.com.br>
2025-05-25 13:33:55 -0300

    Add GET /lists/{listId}/games endpoint to retrieve games based on game list ID

--- src/main/java/br/com/javadeveloper/dslist/controllers/GameListController.java
+++ src/main/java/br/com/javadeveloper/dslist/controllers/GameListController.java
@@ -1,9 +1,12 @@
 package br.com.javadeveloper.dslist.controllers;
 
 import br.com.javadeveloper.dslist.dto.GameListDTO;
+import br.com.javadeveloper.dslist.dto.GameMinDTO;
 import br.com.javadeveloper.dslist.service.GameListService;
+import br.com.javadeveloper.dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,18 @@ public class GameListController {
     @Autowired
     private GameListService gameListService;
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public List<GameListDTO> findAll() {
         List<GameListDTO> result = this.gameListService.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findGames(@PathVariable Long listId) {
+        List<GameMinDTO> result = this.gameService.findByGameList(listId);
         return result;
     }
 }
