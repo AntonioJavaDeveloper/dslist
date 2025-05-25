8b85e09a4636053833225bfc736cdd5c3278c05a
Antonio Gutierrez <antonio@javadeveloper.com.br>
2025-05-25 10:55:27 -0300

    Add findById endpoint in GameController to retrieve game details by ID

--- src/main/java/br/com/javadeveloper/dslist/controllers/GameController.java
+++ src/main/java/br/com/javadeveloper/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package br.com.javadeveloper.dslist.controllers;
 
+import br.com.javadeveloper.dslist.dto.GameDTO;
 import br.com.javadeveloper.dslist.dto.GameMinDTO;
 import br.com.javadeveloper.dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,12 @@ import java.util.List;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
